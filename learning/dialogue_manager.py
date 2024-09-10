19fe68251172c693e1fbbd8c8ff55cfd77fe3cd4
Ethan Ma <em834@cornell.edu>
2024-09-10 15:32:27 -0400

    dialogue manager integrating with gpt client

--- learning/dialogue_manager.py
+++ learning/dialogue_manager.py
@@ -1,3 +1,4 @@
+import json 
 
 class DialogueManager: 
     """
@@ -9,7 +10,23 @@ class DialogueManager:
     def __init__(self, openai_client, user_language):
         self.openai_client = openai_client
         self.user_language = user_language
+        
     
+    def load_scenario(self, difficulty, scenario): 
+        """
+        Loads openai client prompt with user language, difficulty, and scenario
+        
+        Args: 
+            difficulty (string): difficulty of chosen prompt
+            scenario (string): scenario of chosen prompt 
+            
+        Returns: 
+            language prompt 
+        """
+        language = self.user_language
+        scenario = json.load(f'/scenarios/{difficulty}/{scenario}')
+        return self.openai_client.generate_language_prompt(language, scenario)
+        
     
     def start_conversation(self, user_input): 
         system_prompt = f"You are teaching {self.user_language} to a learner. "
