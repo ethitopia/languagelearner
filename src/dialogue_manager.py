25e9edac36b459fbbdafe651cb3de28c9abbb487
Ethan Ma <em834@cornell.edu>
2024-09-09 20:05:34 -0400

    New Directories Added  - languages  - api  etc.

---
+++ src/dialogue_manager.py
@@ -0,0 +1,18 @@
+
+class DialogueManager: 
+    """
+    Manages dialogue with GPT prompts and modeling 
+
+    Args: 
+        scenario (string): string indicating scenario selected 
+    """
+    def __init__(self, scenario): 
+        pass 
+    
+    
+    def get_prompt(self): 
+        pass 
+    
+        
+         
+    
